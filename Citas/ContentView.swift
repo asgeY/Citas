959e832b3300affff8c6ad67b2bf13bd4fde98db
Asgedom <asmow@yahoo.com>
2019-07-12 19:26:46 -0500

    Initial Commit

---
+++ Citas/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Citas
+//
+//  Created by Asge Yohannes on 7/12/19.
+//  Copyright © 2019 Asge Yohannes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
